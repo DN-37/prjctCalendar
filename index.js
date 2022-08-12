59040b9e8907c8aec35cc49dffacf8c4eb64d9a8
Danila Nefedov <dn375@mail.ru>
2022-08-12 17:41:38 +0400

    add function for conclusion array

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ let date  = new Date();
 let year  = date.getFullYear();
 let month = date.getMonth();
 
+let arr = range(getLastDay(year, month));
+
+let firstWeekDay = getFirstWeekDay(year, month);
+let lastWeekDay  = getLastWeekDay(year, month);
+
+let res = chunk(normalize(arr, firstWeekDay, 6 - lastWeekDay), 7));
+console.log(res);
+
+function normalize(arr, left, right) {
+	for (let i = 0; i < left; i++) {
+		arr.unshift('');
+	}
+	for (var i = 0; i < right; i++) {
+		arr.push('');
+	}
+	
+	return arr;
+}
+
+function chunk(arr, n) {
+	let result = [];
+	let count = Math.ceil(arr.length / n);
+	
+	for (let i = 0; i < count; i++) {
+		let elems = arr.splice(0, n);
+		result.push(elems);
+	}
+	
+	return result;
+}
+
 function getLastDay(year, month) {
 	let date = new Date(year, month + 1, 0);
 	return date.getDate();
